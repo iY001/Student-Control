328514c6c9d4b959c7b1647163190db63bd0aec2
iY001 <youssefabdo4005@gmail.com>
2023-12-09 00:36:11 +0200

    --update

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -11,13 +11,13 @@ const Page = () => {
         <a href="#!" className="w-[50px] h-[50px] flex items-center gap-2">
           <img src="https://placeholder.com/50/100" alt="logo" />
           <section>
-          <h1 className="font-bold">STUDENTS</h1>
-          <h1 className=" font-bold  uppercase ">control</h1>
+          <h1 className="font-bold text-main">STUDENTS</h1>
+          <h1 className=" font-bold  uppercase text-main">control</h1>
           </section>
         </a>
 
         <form className="flex flex-col items-center gap-10 w-full h-full mt-[70px]  ">
-          <h1 className="text-3xl ">Login</h1>
+          <h1 className="text-3xl text-main font-bold uppercase">Login</h1>
           
           <div className="relative w-[380px] flex items-center  gap-1 border-b border-[#dddddd63]">
         <IoPersonOutline  size={20} />
@@ -26,10 +26,10 @@ const Page = () => {
         <span className="input-border" />
       </div>
 
-      <div className="relative w-[380px] flex items-center  gap-1  border-b border-[#dddddd63]">
+      <div className="relative w-[380px] flex items-center  gap-1  border-b border-[#dddddd63] ">
         <IoMailOutline  size={20} />
         <input className="input" name="email" required type="text" />
-        <label className="input-label mx-3">Email</label>
+        <label className="input-label  mx-3">Email</label>
         <span className="input-border" />
       </div>
 
@@ -40,9 +40,9 @@ const Page = () => {
         <span className="input-border" />
       </div>
 
-          <button className="bg-[#000000] text-white p-3 lg:w-[60%] w-full  rounded">Log in</button>
+          <button className="bg-main shadow-xl hover:bg-sec duration-300 text-white p-3 lg:w-[60%] w-full  rounded">Log in</button>
 
-          <h1 className="capitalize">forget your password ? <span className="text-[#5891ff] text-xs cursor-pointer">conatct the admin</span></h1>
+          <h1 className="capitalize">forget your password ? <span className="text-sec text-xs cursor-pointer">conatct the admin</span></h1>
         </form>
       </section>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'main': '#592c8c', // Main Color
+        'sec': '#7138b2', // For Hovering - Bright Main
+        'third': "#702283", // Between
+        'forth': "#b135cf", // Very Brigth
+        'light1': "#f3f3f3",
+        'light2': "#f5f5f5",
+      }
     },
   },
   plugins: [],
