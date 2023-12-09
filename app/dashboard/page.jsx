cbe0163433ab4ed2839d2395cdb0d7aa1e7949c7
iY001 <youssefabdo4005@gmail.com>
2023-12-09 09:28:59 +0200

    Dashboard --update #2

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,6 @@
 import { CgProfile } from "react-icons/cg";
 import { CiLogout } from "react-icons/ci";
+import { RxPerson } from "react-icons/rx";
 const Page = () => {
     return (
         <>
@@ -48,6 +49,78 @@ const Page = () => {
 
                 </div> {/* Sections */}
             </aside> {/* Sidebar */}
+            <div className="h-screen ml-[250px] px-8 py-8"> {/* Content - Start */}
+                <div>
+                    <h1 className="uppercase font-bold text-main text-2xl mb-4">DASHBOARD</h1>
+                </div> {/* Title */}
+
+                <div className="w-full flex"> {/* All Boxes - Start */}
+                    <div className="bg-main w-64 h-24 flex justify-between items-center px-8 rounded-lg shadow-xl"> {/* Students Box - Start */}
+                        <section className="bg-sec bg-opacity-70 p-2 rounded-md">
+                            <RxPerson className="text-white text-4xl" />
+                        </section>
+                        <section className="pr-12 text-white">
+                            <h1>600</h1>
+                            <h1>Students</h1>
+                        </section>
+                    </div> {/* Students Box - End */}
+
+                    <div className="bg-main w-64 h-24 flex justify-between items-center px-8 rounded-lg shadow-xl ml-8"> {/* Classes Box - Start */}
+                        <section className="bg-sec bg-opacity-70 p-2 rounded-md">
+                            <RxPerson className="text-white text-4xl" />
+                        </section>
+                        <section className="pr-12 text-white">
+                            <h1>8</h1>
+                            <h1>Classes</h1>
+                        </section>
+                    </div> {/* Classes Box - End */}
+
+                    <div className="bg-main w-64 h-24 flex justify-between items-center px-8 rounded-lg shadow-xl ml-8"> {/* Teachers Box - Start*/}
+                        <section className="bg-sec bg-opacity-70 p-2 rounded-md">
+                            <RxPerson className="text-white text-4xl" />
+                        </section>
+                        <section className="pr-12 text-white">
+                            <h1>20</h1>
+                            <h1>Teachers</h1>
+                        </section>
+                    </div>  {/* Teachers Box - End*/}
+
+                    <div className="bg-main w-64 h-24 flex justify-between items-center px-8 rounded-lg shadow-xl ml-8"> {/* Programming Box - Start */}
+                        <section className="bg-sec bg-opacity-70 p-2 rounded-md">
+                            <RxPerson className="text-white text-4xl" />
+                        </section>
+                        <section className="pr-  text-white">
+                            <h1>200</h1>
+                            <h1 className="text-sm ">Programming</h1>
+                        </section>
+                    </div> {/* Programming Box - End*/}
+                    <div className="bg-main w-64 h-24 flex justify-between items-center px-8 rounded-lg shadow-xl ml-8"> {/* Network Box - Start */}
+                        <section className="bg-sec bg-opacity-70 p-2 rounded-md">
+                            <RxPerson className="text-white text-4xl" />
+                        </section>
+                        <section className="pr-12 text-white">
+                            <h1>200</h1>
+                            <h1>Network</h1>
+                        </section>
+                    </div> {/* Network Box - End */}
+                    <div className="bg-main w-64 h-24 flex justify-between items-center px-8 rounded-lg shadow-xl ml-8"> {/* Telecommunication Box - Start*/}
+                        <section className="bg-sec bg-opacity-70 p-2 rounded-md">
+                            <RxPerson className="text-white text-4xl" />
+                        </section>
+                        <section className="p-2 text-white">
+                            <h1>200</h1>
+                            <h1>Telecommunication</h1>
+                        </section>
+                    </div> {/* Telecommunication Box - End */}
+                </div> {/* All Boxes - End */}
+
+                <div>
+                    <div> {/* Chart & Table - Start */}
+                        <h1 className="uppercase font-bold text-main text-2xl my-6 ">Leaderboard</h1>
+                    </div> {/* Title */}
+
+                </div> {/* Chart & Table     - End */}
+            </div> {/* Content - End*/}
         </ >
     );
 }
